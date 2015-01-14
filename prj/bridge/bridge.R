311a3b8ce1131ec70d6cc3719180c6d40d5dd567
kiwon <wh1988ha@gmail.com>
2015-01-14 12:55:09 +0900

    temp

--- prj/bridge/bridge.R
+++ prj/bridge/bridge.R
@@ -9,13 +9,30 @@ require(xlsx)
 fileList <-list.files(path = "./data/");    # get file list from data folder
 
 fileList.len <-length(fileList);    # get number of files.
-tables <- numeric(fileList.len);    # declare
-
+tables <- list(fileList.len);    # declare
+len.max <-0;    # default data row lwngth
 for (i in 1:fileList.len) {
 #     print(fileList[i]);
     dir <- "./data/";
     dir.temp <-paste(dir, fileList[i], sep = "");   # 
-    tables[i] <- read.xlsx(dir.temp, sheetName = "Sheet1");
+    tables[i] <- list(tbl=read.xlsx(dir.temp, sheetName = "Sheet1"));
+    temp <- read.xlsx(dir.temp, sheetName = "Sheet1");
 #     tables[i] <- readWorksheet(loadWorkbook(dir.temp),sheet=1)
+    len.temp <- nrow(tables[[i]]);
+    if(len.max < len.temp){ # find the maximum row length of data
+        len.max <- len.temp;
+    }
+    
+    len.max    
+    if(i==4){
+        break;
+    }
+    print(i);
 }
 
+out <- data.frame (id=1:len.max)
+
+for(j in 1:fileList.len){
+    out$s <- tables[[j]][3]
+}
+# add 0 data amount len.max into data which has less length.
